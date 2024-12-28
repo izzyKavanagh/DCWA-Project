5faa241169ac2d0052a31a65fb39a98bf1f7ecb1
izzyK <g00424523@gmail.com>
2024-12-28 20:19:28 +0000

    add a function to update the new student details in the database

--- mySqlDao.js
+++ mySqlDao.js
@@ -30,19 +30,39 @@ var getStudents = function()
 
 var getStudentBySID = function (sid) {
     return new Promise((resolve, reject) => {
-        const query = "SELECT * FROM student WHERE sid = ?";  // Parameterized query
-        pool.query(query, [sid])  // Pass `id` directly as a parameter
+        const query = "SELECT * FROM student WHERE sid = ?"; 
+        pool.query(query, [sid]) 
             .then((results) => {
                 if (results.length > 0) {
-                    resolve(results[0]);  // Return the first matching student
+                    resolve(results[0]);  
                 } else {
                     reject(`Student with SID ${sid} not found`);
                 }
             })
             .catch((err) => {
-                reject(err);  // Return error if query fails
+                reject(err); 
             });
     });
   };
 
-module.exports = {getStudents, getStudentBySID}
+  const updateStudent = (sid, updatedStudent) => {
+    return new Promise((resolve, reject) => {
+        let query = "UPDATE student SET name = ?, age = ? WHERE sid = ?";
+        let params = [updatedStudent.name, updatedStudent.age, sid];
+
+        console.log("Executing query:", query, params);
+
+        pool.query(query, params) 
+            .then((results) => {
+                if (results.affectedRows === 0) {
+                    return reject(new Error("Student ID not found."));
+                }
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+module.exports = {getStudents, getStudentBySID, updateStudent}
