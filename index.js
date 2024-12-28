0457cf0d39e5e5ee5606915fdc2905c5884a6951
izzyK <g00424523@gmail.com>
2024-12-28 14:22:33 +0000

    create route to display student details

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ app.set('view engine', 'ejs');
 app.listen(3004, () => {
     console.log("Running on port 3000");
 })
+
+app.get("/", (req,res) => {
+    mySqlDao.getStudents()
+        .then((data) => {
+            res.render(data)
+        })
+        .catch((error)=> {
+            res.send(error)
+        })
+})
