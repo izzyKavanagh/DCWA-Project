ac49287965f5227eb5b54de7f9fa8dd4df772e63
izzyK <g00424523@gmail.com>
2024-12-28 19:02:10 +0000

    add routing for edit student page

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require ('express')
 let ejs = require('ejs')
 var app = express()
 var mySqlDao = require('./mySqlDao');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.set('view engine', 'ejs');
 
 app.listen(3004, () => {
-    console.log("Running on port 3000");
+    console.log("Running on port 3004");
 })
 
 app.get("/", (req,res) => {
@@ -28,5 +30,13 @@ app.get("/students/add", (req,res) => {
 })
 
 app.get("/students/edit/:sid", (req,res) => {
-    res.render("editStudent");
+    let sid = req.params.sid;
+    mySqlDao.getStudentBySID(sid)
+        .then((student) => {
+            res.render("editStudent", { student: student });
+             console.log("Student: ", student)
+        })
+        .catch((error) => {
+            res.send(error);
+        });
 })
